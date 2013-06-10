a710da21186778937162caff9dfe37696ff5fce4
Steve Lord <steve@canderous.local>
2013-06-10 23:49:00 +0100

    God I'm a moron. Missing sudos fixed

--- ec2/instxlt.sh
+++ ec2/instxlt.sh
@@ -7,8 +7,8 @@ EOF
 
 gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
-apt-get update
-apt-get install tor deb.torproject.org-keyring obfsproxy
+sudo apt-get update
+sudo apt-get install tor deb.torproject.org-keyring obfsproxy
 sudo apt-get upgrade -y
 sudo apt-get install openswan xl2tpd ppp -y
 sudo cp ./xlt.sh /etc/xlt.sh
