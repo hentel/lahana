1063781c54c08ff0f62c18ff5338bbeaec54bb5e
Steve Lord <steve@canderous.local>
2013-06-16 13:52:44 +0100

    Fixed a couple of bugs with apt-repository stuff

--- ec2/instxlt.sh
+++ ec2/instxlt.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-cat >> /etc/apt/sources.list <<EOF
+sudo cat >> /etc/apt/sources.list <<EOF
 deb     http://deb.torproject.org/torproject.org precise main
 deb     http://deb.torproject.org/torproject.org experimental-precise main
 EOF
 
-gpg --keyserver keys.gnupg.net --recv 886DDD89
-gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+sudo gpg --keyserver keys.gnupg.net --recv 886DDD89
+sudo gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install tor deb.torproject.org-keyring obfsproxy
 sudo apt-get upgrade -y
